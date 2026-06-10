245967de9847e3cb0ee7a32010391713da8eedd5
nithinreddybommireddy <nithinreddybommireddy@gmail.com>
2026-06-10 22:25:56 +0530

    feat:Product Repository

--- src/main/java/com/trainingmug/ecommerce/repository/ProductRepository.java
+++ src/main/java/com/trainingmug/ecommerce/repository/ProductRepository.java
@@ -1,6 +1,5 @@
 package com.trainingmug.ecommerce.repository;
 
-
 import com.trainingmug.ecommerce.model.Product;
 import com.trainingmug.ecommerce.util.CsvReader;
 import org.springframework.stereotype.Repository;
@@ -8,17 +7,19 @@ import org.springframework.stereotype.Repository;
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
+
 @Repository
 public class ProductRepository {
+
     private final CsvReader csvReader;
     private final List<Product> products;
 
     public ProductRepository(CsvReader csvReader) throws IOException {
         this.csvReader = csvReader;
         this.products = this.csvReader.getProductFromCsv();
-
     }
-    public Product save(Product product){
+
+    public Product save(Product product) {
         this.products.add(product);
         return product;
     }
@@ -28,12 +29,21 @@ public class ProductRepository {
     }
 
     public Optional<Product> findById(int id) {
-        return products.stream().filter(product -> product.getId() == id).findFirst();
-
+        return products.stream()
+                .filter(product -> product.getId() == id)
+                .findFirst();
     }
 
     public Product update(Product product) {
-        return (Product) products.stream().filter(p -> p.getId() == product.getId()).map(p -> {
+
+        Optional<Product> existingProduct = products.stream()
+                .filter(p -> p.getId() == product.getId())
+                .findFirst();
+
+        if (existingProduct.isPresent()) {
+
+            Product p = existingProduct.get();
+
             p.setName(product.getName());
             p.setMaxRetailPrice(product.getMaxRetailPrice());
             p.setDiscountPercentage(product.getDiscountPercentage());
@@ -41,14 +51,14 @@ public class ProductRepository {
             p.setCompany(product.getCompany());
             p.setCategory(product.getCategory());
             p.setManufactureYear(product.getManufactureYear());
+
             return p;
-        });
+        }
+
+        return null;
     }
 
     public void delete(int id) {
         products.removeIf(p -> p.getId() == id);
     }
 }
-
-
-
