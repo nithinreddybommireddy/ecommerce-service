ff0e13094615302683d89731c9d6cc5a522d3644
nithinreddybommireddy <nithinreddybommireddy@gmail.com>
2026-06-15 20:25:31 +0530

    feat:Customer Controller

--- src/main/java/com/trainingmug/ecommerce/controller/CustomerController.java
+++ src/main/java/com/trainingmug/ecommerce/controller/CustomerController.java
@@ -1,8 +1,9 @@
 package com.trainingmug.ecommerce.controller;
 
-import com.trainingmug.ecommerce.exception.CustomerExistsException;
-import com.trainingmug.ecommerce.exception.CustomerNotFoundException;
-import com.trainingmug.ecommerce.model.Customer;
+import com.trainingmug.ecommerce.dto.request.LoginRequestDto;
+import com.trainingmug.ecommerce.dto.request.SignUpRequestDto;
+import com.trainingmug.ecommerce.dto.response.CustomerResponseDto;
+import com.trainingmug.ecommerce.entity.Customer;
 import com.trainingmug.ecommerce.service.CustomerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -20,18 +21,14 @@ public class CustomerController {
 
     //CRUD operations (End Points)
     //Save ( POST -> body)
-    @PostMapping
-    public ResponseEntity<?> save(@RequestBody Customer customer) {
+    @PostMapping("/signup")
+    public ResponseEntity<CustomerResponseDto> signup(@RequestBody SignUpRequestDto signupRequestDto) {
         //1. Throw CustomerExistsException if customer exists
         //2. save customer
         //3. return saved customer
-        try {
-            return ResponseEntity.status(HttpStatus.CREATED).body(customerService.save(customer));
-        } catch (CustomerExistsException ex) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
-        } catch (Exception ex) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
-        }
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(customerService.register(signupRequestDto));
+
         //ResponseEntity Types
         /*
         201 Created -> ResponseEntity<Customer>
@@ -41,43 +38,28 @@ public class CustomerController {
     }
 
     @GetMapping
-    public ResponseEntity<List<Customer>> getAll(){
+    public ResponseEntity<List<Customer>> getAll() {
         //GET -> 200 Ok
         //ResonseEntity.status(HttpStatus.OK).body(customerService.getAll());
         return ResponseEntity.ok(customerService.getAll());
     }
+
     //http://localhost:8080/api/customers/1
     @GetMapping("/{id}")
-    public ResponseEntity<?> getById(@PathVariable int id){
-        try {
-            return ResponseEntity.ok(customerService.getById(id));
-        } catch(CustomerNotFoundException ex){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
-        } catch(Exception ex){
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
-        }
+    public ResponseEntity<Customer> getById(@PathVariable int id) {
+        return ResponseEntity.ok(customerService.getById(id));
     }
+
     @PutMapping
-    public ResponseEntity<?> update(@RequestBody Customer customer){
-        try {
-            return ResponseEntity.ok(customerService.update(customer));
-        } catch(CustomerNotFoundException ex){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
-        } catch(Exception ex){
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
-        }
+    public ResponseEntity<Customer> update(@RequestBody Customer customer) {
+        return ResponseEntity.ok(customerService.update(customer));
+
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> delete(@PathVariable int id){
-        try {
-            customerService.delete(id);
-            return ResponseEntity.noContent().build();
-        } catch(CustomerNotFoundException ex){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
-        } catch(Exception ex){
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
-        }
+    public ResponseEntity<Void> delete(@PathVariable int id) {
+        customerService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
     /*
@@ -85,6 +67,11 @@ public class CustomerController {
     @DeleteMapping
     public ResponseEntity<?> deleteById(@RequestParam int id){}
      */
+
+    @PostMapping("/login")
+    public ResponseEntity<CustomerResponseDto> login(@RequestBody LoginRequestDto loginRequestDto) {
+        return ResponseEntity.ok(customerService.login(loginRequestDto));
+    }
 
 
 }
