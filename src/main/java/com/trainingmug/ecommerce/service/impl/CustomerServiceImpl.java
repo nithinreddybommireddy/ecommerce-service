1693a364e65e281e37df21cd732dc87e0be3f6e9
nithinreddybommireddy <nithinreddybommireddy@gmail.com>
2026-06-15 20:22:31 +0530

    feat:Customer Service Implementation

--- src/main/java/com/trainingmug/ecommerce/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/trainingmug/ecommerce/service/impl/CustomerServiceImpl.java
@@ -1,30 +1,56 @@
 package com.trainingmug.ecommerce.service.impl;
 
+import com.trainingmug.ecommerce.dto.request.LoginRequestDto;
+import com.trainingmug.ecommerce.dto.request.SignUpRequestDto;
+import com.trainingmug.ecommerce.dto.response.CustomerResponseDto;
 import com.trainingmug.ecommerce.enums.Status;
 import com.trainingmug.ecommerce.exception.CustomerExistsException;
 import com.trainingmug.ecommerce.exception.CustomerNotFoundException;
-import com.trainingmug.ecommerce.model.Customer;
+import com.trainingmug.ecommerce.entity.Customer;
 import com.trainingmug.ecommerce.repository.CustomerRepository;
 import com.trainingmug.ecommerce.service.CustomerService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class CustomerServiceImpl implements CustomerService {
 
     private final CustomerRepository customerRepository;
+    private final ModelMapper modelMapper;
+
+
     @Override
-    public Customer save(Customer customer) throws CustomerExistsException {
-        customerRepository.findByEmail(customer.getEmail()).ifPresent(c -> {
-            throw new CustomerExistsException("Customer exists with email: " + customer.getEmail() );
+    public CustomerResponseDto register(SignUpRequestDto signupRequestDto) throws CustomerExistsException {
+        customerRepository.findByEmail(signupRequestDto.getEmail()).ifPresent(c -> {
+            throw new CustomerExistsException("Customer exists with email: " + signupRequestDto.getName());
         });
+
+//        Customer customer = Customer.builder()
+//                .name(signupRequestDto.getName())
+//                .email(signupRequestDto.getEmail())
+//                .password(signupRequestDto.getPassword())
+//                .phone(signupRequestDto.getPhone())
+//                .gender(signupRequestDto.getGender())
+//                .build();
+        Customer customer = modelMapper.map(signupRequestDto, Customer.class);
+        //Customer Entity
         customer.setStatus(Status.ACTIVE);
         customer.setCreatedAt(LocalDateTime.now());
         customer.setLastLoggedInAt(LocalDateTime.now());
-        return customerRepository.save(customer);
+        /*Customer savedCustomer = customerRepository.save(customer);
+
+        return CustomerResponseDto.builder()
+                .id(savedCustomer.getId())
+                .name(savedCustomer.getName())
+                .email(savedCustomer.getEmail())
+                .status(savedCustomer.getStatus())
+                .build();*/
+        return modelMapper.map(customerRepository.save(customer), CustomerResponseDto.class);
     }
 
     @Override
@@ -35,7 +61,7 @@ public class CustomerServiceImpl implements CustomerService {
     @Override
     public Customer update(Customer customer) throws CustomerNotFoundException {
         customerRepository.findById(customer.getId()).orElseThrow(() -> new CustomerNotFoundException("Customer not found with id: " + customer.getId()));
-        return customerRepository.update(customer);
+        return customerRepository.save(customer);
     }
 
     @Override
@@ -47,5 +73,20 @@ public class CustomerServiceImpl implements CustomerService {
     public void delete(int id) throws CustomerNotFoundException {
         customerRepository.findById(id).orElseThrow(() -> new CustomerNotFoundException("Customer not found with id: " + id));
         customerRepository.deleteById(id);
+    }
+
+    @Override
+    public CustomerResponseDto login(LoginRequestDto loginRequestDto) throws CustomerNotFoundException {
+
+        /*Customer customer = customerRepository.findDistinctByEmailAndPassword(loginRequestDto.getEmail(), loginRequestDto.getPassword()).orElseThrow(() -> new CustomerNotFoundException("Customer not found with email: " + loginRequestDto.getEmail()));
+
+        return CustomerResponseDto.builder()
+                .id(customer.getId())
+                .name(customer.getName())
+                .email(customer.getEmail())
+                .status(customer.getStatus())
+                .build();*/
+        return modelMapper.map(customerRepository.findDistinctByEmailAndPassword(loginRequestDto.getEmail(), loginRequestDto.getPassword()).orElseThrow(() -> new CustomerNotFoundException("Customer not found with email: " + loginRequestDto.getEmail())), CustomerResponseDto.class);
+
     }
 }
