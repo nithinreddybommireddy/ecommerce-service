944c74ddc28e4dc303e216cfb37e2298c1953a07
nithinreddybommireddy <nithinreddybommireddy@gmail.com>
2026-06-15 20:21:28 +0530

    feat:Customer Service

--- src/main/java/com/trainingmug/ecommerce/service/CustomerService.java
+++ src/main/java/com/trainingmug/ecommerce/service/CustomerService.java
@@ -1,18 +1,24 @@
 package com.trainingmug.ecommerce.service;
 
+import com.trainingmug.ecommerce.dto.request.LoginRequestDto;
+import com.trainingmug.ecommerce.dto.request.SignUpRequestDto;
+import com.trainingmug.ecommerce.dto.response.CustomerResponseDto;
 import com.trainingmug.ecommerce.exception.CustomerExistsException;
 import com.trainingmug.ecommerce.exception.CustomerNotFoundException;
-import com.trainingmug.ecommerce.model.Customer;
+import com.trainingmug.ecommerce.entity.Customer;
 
 import java.util.List;
 
 public interface CustomerService {
-    Customer save(Customer customer) throws CustomerExistsException;
+    CustomerResponseDto register(SignUpRequestDto signupRequestDto) throws CustomerExistsException;
+
     Customer getById(int id) throws CustomerNotFoundException;
+
     Customer update(Customer customer) throws CustomerNotFoundException;
+
     List<Customer> getAll();
+
     void delete(int id) throws CustomerNotFoundException;
 
-
-
+    CustomerResponseDto login(LoginRequestDto loginRequestDto) throws CustomerNotFoundException;
 }
