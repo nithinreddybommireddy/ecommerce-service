e2655ff4ba20452d33ad4f59dc08543a26a6985a
nithinreddybommireddy <nithinreddybommireddy@gmail.com>
2026-06-15 20:26:00 +0530

    feat:Customer Repository

--- src/main/java/com/trainingmug/ecommerce/repository/CustomerRepository.java
+++ src/main/java/com/trainingmug/ecommerce/repository/CustomerRepository.java
@@ -1,45 +1,30 @@
 package com.trainingmug.ecommerce.repository;
 
-import com.trainingmug.ecommerce.model.Customer;
+import com.trainingmug.ecommerce.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class CustomerRepository {
-    //This layer will be replaced with db operations
+public interface CustomerRepository extends JpaRepository<Customer,Integer> {
+    //JpaRepository
+    /*
+    CRUD
+    save,findById, delete, findAll
 
-    private List<Customer> customers;
-
-    public CustomerRepository(){
-        this.customers = new ArrayList<>();
-    }
-
-    public Customer save(Customer customer){
-        this.customers.add(customer);
-        return customer;
-    }
-    public List<Customer> findAll(){
-        return this.customers;
-    }
-    public Optional<Customer> findById(int id){
-        return this.customers.stream().filter(c -> c.getId() == id).findFirst();
-    }
-    public Optional<Customer> findByEmail(String email){
-        return this.customers.stream().filter(c -> c.getEmail().equals(email)).findFirst();
-    }
-    public Customer update(Customer customer){
-        this.customers.replaceAll(c -> c.getId() == customer.getId() ? customer : c);
-        return customer;
-    }
-    public void deleteById(int id){
-        this.customers.removeIf(c -> c.getId() == id);
-    }
-    public void delete(Customer customer){
-        this.customers.remove(customer);
-    }
+    Derived Methods
+    findBy, count, exists
+     */
+    //SQL Query
+    //select * from customer where email = ?
+    Optional<Customer> findByEmail(String email);
+    Optional<Customer> findDistinctByEmailAndPassword(String email, String password);
+    List<Customer> findByCreatedAtBetween(LocalDateTime start, LocalDateTime end);
+    List<Customer> findByNameLike(String namePattern);
+    List<Customer> findByNameOrderByCreatedAtDesc(String name);
 
 
 }
